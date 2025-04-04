edc301af56b01de1d7475a8611460d3a5a1deea2
7UR7L3 <m7ur7l3@yahoo.com>
2025-04-04 15:37:36 -0600

    fouc

--- site/js/theme.js
+++ site/js/theme.js
@@ -1,5 +1,18 @@
 /* js/theme.js */
 
+function applyTheme(theme) {
+  metaColorSchemes = {
+    light: "light",
+    dark: "dark"
+  };
+  const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+  theme = theme ? theme : systemTheme;
+  const colorScheme = theme in metaColorSchemes ? metaColorSchemes[theme] : systemTheme;
+  
+  document.documentElement.setAttribute("data-theme", theme)
+  document.querySelector('meta[name="color-scheme"]').content = colorScheme;
+}
+
 /**
  * setTheme(themeName) sets a custom theme when a non-empty string is passed.
  * If no value is provided, it unsets the custom theme so the system preference is used.
@@ -14,7 +27,6 @@ function setTheme(themeName) {
   applyTheme(localStorage.getItem("theme"));
   updateSystemThemeListener();
 }
-
 
 const darkQuery = window.matchMedia("(prefers-color-scheme: dark)");
 let listenerAttached = false;
@@ -32,6 +44,9 @@ function updateSystemThemeListener() {
     }
   }
 }
+
+applyTheme(localStorage.getItem("theme"));
+
 // Ensure updateSystemThemeListener() is called at least once on load (e.g. if using system theme preference, watch it if it changes)
 updateSystemThemeListener();
 
